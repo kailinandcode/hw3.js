a1a91dc5a6e48d5094eaf7924e64a841dae806ab
Kailin Li <35666977+kailinandcode@users.noreply.github.com>
2018-02-05 20:11:17 -0800

    Update concentricCircles.js

--- concentricCircles.js
+++ concentricCircles.js
@@ -1,1 +1,13 @@
 //Concentric Circles
+
+noFill();
+
+function concentricCircles() {
+  var radius = height;
+  while (radius > 10) {
+    ellipse(width/2, width/2, radius, radius);
+    radius = radius - 10;
+  }
+}
+
+concentricCircles();
